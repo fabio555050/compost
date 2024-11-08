d3e07d14d477309db657ceae62fad0b4786be4f4
fabio555050 <2101440@aluno.univesp.br>
2024-11-07 21:51:24 -0300

    correção de ambiguidade

--- src/main/java/com/api/projeto/integrador/controllers/ControleDeGadoController.java
+++ src/main/java/com/api/projeto/integrador/controllers/ControleDeGadoController.java
@@ -15,7 +15,6 @@ import com.api.projeto.integrador.models.ControleDeGado;
 import com.api.projeto.integrador.services.ControleDeGadoService;
 import java.util.List;
 import java.util.Map;
-import antlr.collections.List;
 
 @RestController
 @RequestMapping("/animal")
